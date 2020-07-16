619d0968959f247c59126df74dea9eaacc68f64f
Josue Brito <jbrito@opstrax.com>
2020-07-16 16:43:30 -0500

    implemented custom font

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,48 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
+  appitems = [
+    {
+      label: 'Home',
+      faIcon: 'icon-dashboard'
+    },
+    {
+      label: 'Colaboradores',
+      faIcon: 'icon-collaborators'
+    },
+    {
+      label: 'Logeo',
+      faIcon: 'icon-logeo'
+    },
+    {
+      label: 'Anuncios',
+      faIcon: 'icon-announcements'
+    },
+    {
+      label: 'Solicitudes',
+      faIcon: 'icon-requests'
+    },
+    {
+      label: 'Reportes',
+      faIcon: 'icon-reports',
+    },
+    {
+      label: 'Configuracion',
+      faIcon: 'icon-configurations',
+    },
+    {
+      label: 'Logout',
+      faIcon: 'icon-logout',
+    }
+  ];
+  config = {
+    paddingAtStart: true,
+    classname: 'my-custom-class',
+    listBackgroundColor: '#377ccb',
+    fontColor: '#cae1ff',
+    backgroundColor: '#377ccb',
+    selectedListFontColor: 'red',
+  };
   title = 'logeofont';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+/* Import the module*/
+import { NgMaterialMultilevelMenuModule } from 'ng-material-multilevel-menu';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgMaterialMultilevelMenuModule
   ],
   providers: [],
   bootstrap: [AppComponent]
